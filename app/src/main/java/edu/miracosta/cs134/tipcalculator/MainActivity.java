5f37bf0b6afaa6d0e9a84fea8992d0af0a28012b
4804W04 <4804W04@usa.miracosta.cc.ca.us>
2020-02-04 11:12:21 -0800

    Initial commit

---
+++ app/src/main/java/edu/miracosta/cs134/tipcalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.miracosta.cs134.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
